01307c8eb3ede750d2906b77ed293cb9cf965d45
tomas <tomasrenaa@gmail.com>
2020-05-04 16:16:20 +0200

    .]

--- src/main.js
+++ src/main.js
@@ -3,6 +3,26 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+
+
+import * as firebase from 'firebase'
+
+Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCj7EeQNc8NPJd90ewqBCt52rJGkcVmTAY",
+  authDomain: "fir-auth-logrocket.firebaseapp.com",
+  databaseURL: "https://fir-auth-logrocket.firebaseio.com",
+  projectId: "fir-auth-logrocket",
+  storageBucket: "fir-auth-logrocket.appspot.com",
+  messagingSenderId: "379373513767",
+  appId: "1:379373513767:web:628d0b8ad47032fca43202"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
+import Register from '../views/Register.vue'
+
 Vue.use(VueRouter)
 
   const routes = [
@@ -17,8 +19,11 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  { path: '/register', component: Register}
 ]
+
+
 
 const router = new VueRouter({
   mode: 'history',
